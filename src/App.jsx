7d9ff47bfd6603d5fb0781abb8542eb01dc34924
isalah95 <isalah@delvehealth.com>
2025-02-13 19:17:19 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Projects from './Pages/Projects';
 import Contact from './Pages/Contact';
+import Resume from "./Pages/Resume";
 
 function App() {
   return (
