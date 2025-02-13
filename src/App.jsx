1c94b4fb5d04341cd4184d2b36dcc0100d896c1f
isalah95 <isalah@delvehealth.com>
2025-02-13 19:52:36 +0300

    Fix homepage loading issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
-import Projects from './Pages/Projects';
-import Contact from './Pages/Contact';
-import Resume from "./Pages/Resume";
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Resume from './Pages/Resume';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="https://islams95.github.io/My-portfolio/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} /> 
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
